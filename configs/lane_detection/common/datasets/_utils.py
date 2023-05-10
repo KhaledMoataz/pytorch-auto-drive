11552f019ddd1f2bf7a4a644ebec889d6bc1bcee
Khaled Moataz <khaled.nassar99@eng-st.cu.edu.eg>
2023-05-11 01:20:28 +0300

    change tusimple path

--- configs/lane_detection/common/datasets/_utils.py
+++ configs/lane_detection/common/datasets/_utils.py
@@ -1,3 +1,3 @@
-CULANE_ROOT = '../../dataset/culane'
-TUSIMPLE_ROOT = '../../dataset/tusimple'
-LLAMAS_ROOT = '../../dataset/llamas'
+CULANE_ROOT = "../../dataset/culane"
+TUSIMPLE_ROOT = "/kaggle/input/tusimple"
+LLAMAS_ROOT = "../../dataset/llamas"
