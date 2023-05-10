cf481b472f623babb5dc2105d08b65183ca6a223
Khaled Moataz <khaled.nassar99@eng-st.cu.edu.eg>
2023-05-11 01:31:04 +0300

    Change paths for lists dir because of readonly kaggle

--- configs/lane_detection/common/datasets/_utils.py
+++ configs/lane_detection/common/datasets/_utils.py
@@ -1,3 +1,4 @@
 CULANE_ROOT = "../../dataset/culane"
 TUSIMPLE_ROOT = "/kaggle/input/tusimple"
+TUSIMPLE_LISTS_ROOT = "/kaggle/working/pytorch-auto-drive/lists"
 LLAMAS_ROOT = "../../dataset/llamas"
--- tools/tusimple_list_convertor.py
+++ tools/tusimple_list_convertor.py
@@ -4,21 +4,31 @@
 import os
 
 from importmagician import import_from
-with import_from('./'):
+
+with import_from("./"):
     from configs.lane_detection.common.datasets._utils import TUSIMPLE_ROOT as base
-root = os.path.join(base, 'lists')
-old_file_names = ['list6_train.txt', 'list6_val.txt', 'list6_val.txt', 'list_test.txt']  # 6 lanes (actually <=5)
+    from configs.lane_detection.common.datasets._utils import TUSIMPLE_LISTS_ROOT
+root = os.path.join(base, "lists")
+old_file_names = [
+    "list6_train.txt",
+    "list6_val.txt",
+    "list6_val.txt",
+    "list_test.txt",
+]  # 6 lanes (actually <=5)
 # old_file_names = ['list_train.txt', 'list_val.txt', 'list_val.txt', 'list_test.txt']  # 4 lanes
-new_file_names = ['train.txt', 'valfast.txt', 'val.txt', 'test.txt']
+new_file_names = ["train.txt", "valfast.txt", "val.txt", "test.txt"]
 for i in range(len(old_file_names)):
-    file_name = os.path.join(root, old_file_names[i])
-    with open(file_name, 'r') as f:
+    file_name = os.path.join(TUSIMPLE_LISTS_ROOT, old_file_names[i])
+    with open(file_name, "r") as f:
         temp = f.readlines()
     for x in range(len(temp)):
-        if new_file_names[i] == 'test.txt' or new_file_names[i] == 'val.txt':
-            temp[x] = temp[x][temp[x].find('clips/') + 6: temp[x].find('.jpg')] + '\n'
+        if new_file_names[i] == "test.txt" or new_file_names[i] == "val.txt":
+            temp[x] = temp[x][temp[x].find("clips/") + 6 : temp[x].find(".jpg")] + "\n"
         else:
-            temp[x] = temp[x][temp[x].find('clips/') + 6: temp[x].find('.jpg')] + temp[x][temp[x].find('.png') + 4:]
-    file_name = os.path.join(root, new_file_names[i])
-    with open(file_name, 'w') as f:
+            temp[x] = (
+                temp[x][temp[x].find("clips/") + 6 : temp[x].find(".jpg")]
+                + temp[x][temp[x].find(".png") + 4 :]
+            )
+    file_name = os.path.join(TUSIMPLE_LISTS_ROOT, new_file_names[i])
+    with open(file_name, "w") as f:
         f.writelines(temp)
